4a399761b592ee378a768c781774f625d5d91f01
Efren Tovar Silva <117499555+ejts@users.noreply.github.com>
2025-11-08 05:50:17 -0300

    calm Commit Message

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://evgykiyuirkjxppqvfjt.supabase.co'; 
 // CLAVE MÁS RECIENTE:
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Z3lraXl1aXJranhwcXF2Zmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NjI3NTAsImV4cCI6MjA3ODEzODc1MH0.6wuT3NtmeRBHxkZxCTwrrGzJPjWEw39WIg9qOhVtIHs'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Z3lraXl1aXJranhwcHF2Zmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NjI3NTAsImV4cCI6MjA3ODEzODc1MH0.6wuT3NtmeRBHxkZxCTwrrGzJPjWEw39WIg9qOhVtIHs'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
