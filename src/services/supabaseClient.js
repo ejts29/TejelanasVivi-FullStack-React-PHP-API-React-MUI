c1444dad4d959f339bb4fafc7bb35c5b8bb3e716
Efren Tovar Silva <117499555+ejts@users.noreply.github.com>
2025-11-08 05:46:08 -0300

    jhjh

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,9 @@
+// src/services/supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js';
 
-// Clave completa y correcta
 const supabaseUrl = 'https://evgykiyuirkjxppqvfjt.supabase.co'; 
+// CLAVE MÁS RECIENTE:
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Z3lraXl1aXJranhwcXF2Zmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NjI3NTAsImV4cCI6MjA3ODEzODc1MH0.6wuT3NtmeRBHxkZxCTwrrGzJPjWEw39WIg9qOhVtIHs'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
